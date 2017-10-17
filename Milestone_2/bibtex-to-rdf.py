f85a802912cafdd1a838be3c20ad4ff171ed366f
Clokman <john.c.lokman@gmail.com>
2017-10-17 12:01:35 +0200

    Testing VCS in IDE

--- Milestone_2/bibtex-to-rdf.py
+++ Milestone_2/bibtex-to-rdf.py
@@ -1,15 +1,2 @@
-# This Python file uses the following encoding: utf-8
-# The line above is a magic comment and it should not be removed.
+import bibtex_parser
 
-import bibtexparser
-with open("C:\\Users\\Clokman\\Google Drive\\__Projects__\\Code\\KAD\\Milestone_2\\test.bib") as bibtex_file:
-    bib_database = bibtexparser.load(bibtex_file)
-
-
-# Print specific part of the example
-for each_entry in bib_database.entries:
-    print (str(each_entry['author'] + " has_article ") + each_entry['title'] + ' .')
-    #try:
-    #    print (each_entry["ENTRYTYPE"])
-    #except:
-    #    pass
