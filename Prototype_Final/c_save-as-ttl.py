162b6016ccde80d5eb708062c8a5395ced6ebf0d
Clokman <john.c.lokman@gmail.com>
2017-10-24 16:52:56 +0200

    Refactoring completed

--- Prototype_Final/c_save-as-ttl.py
+++ Prototype_Final/c_save-as-ttl.py
@@ -1,32 +1,7 @@
-from b_bibtex_to_rdf import triples_list
+from b_ttl_triple_creator import triples_list
 print (triples_list)
 
-file_obj = open("Output//pure_bib_limited_0.3.ttl", "w")
+file_obj = open("Output//pure_bib_limited_0.4.ttl", "w")
 for each_triple in triples_list:
     file_obj.write(each_triple)
     file_obj.write('\n')
-
-
-
-#from rdflib import Graph, RDF, Namespace, Literal, URIRef
-#
-#def serialize():
-#    print Graph().serialize(format='turtle')
-#
-#
-#def save(filename):
-#    with open(filename, 'w') as f:
-#        Graph().serialize(f, format='turtle')
-#
-#
-#def load(filename):
-#    with open(filename, 'r') as f:
-#        Graph().load(f, format='turtle')
-#
-#load("pure.ttl")
-#
-#for each_triple in triples_list:
-#    Graph().add(each_triple)
-#
-#
-#
